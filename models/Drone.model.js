60a138a77fcbeab528e92401572a69503c8b1719
Ana Belen Jover Ballester <anab.jover@gmail.com>
2022-05-09 19:06:26 +0200

    almost done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const {Schema, model} = require ("mongoose");
+
+const droneSchema = new Schema({
+    image: {
+        data: String, 
+    },
+    name: {
+        type: String,
+    },
+    propellers: {
+        type: Number,
+    },
+    maxSpeed: {
+        type: Number,
+    }
+})
+
+const DroneModel = model("drone", droneSchema)
+
+module.exports = DroneModel;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,36 @@
 // Iteration #1
+const drones = [
+    {
+    image: ("/images/drone1.jpg"),
+    name: "Creeper XL 500",
+    propellers: 3,
+    maxSpeed: 12
+},
+  {
+    image: ("/images/drone2.jpg"),
+    name: "Racer 57",
+    propellers: 4,
+    maxSpeed: 20
+},
+  {
+    image: ("/images/drone3.jpg"),
+    name: "Courier 3000i",
+    propellers: 6,
+    maxSpeed: 18
+}
+]
+
+require("../db")
+
+const DroneModel = require("../models/Drone.model.js")
+
+const addDrone = async () => {
+    try {
+        await DroneModel.create(drones)
+        console.log("todos los Drones han sido añadidos.")
+    } catch(err){
+        next(err)
+    }
+}
+addDrone()
+// mongoose.connection.close()
